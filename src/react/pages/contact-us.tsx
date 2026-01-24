ee00eded6c232eb8c5d7f4cfb0d91283b107c249
Charlie <hammersleycharlie@gmail.com>
2026-01-24 14:30:51 -0500

    Update contact-us.tsx

--- src/react/pages/contact-us.tsx
+++ src/react/pages/contact-us.tsx
@@ -15,12 +15,12 @@ export class ContactUs extends React.Component<Props, State> {
 				<PageHeading>Contact Us!</PageHeading>
 				<p>We'd love to hear from you! You can reach out to our team in a number of ways, including:</p>
 				<ul>
-					<li>Email us at <a href="mailto:raptors1711@gmail.com">raptors1711@gmail.com</a>.</li>
-					<li>Find us on Instagram at <a href="https://instagram.com/raptors1711">@raptors1711</a></li>
-					<li>Follow us on TikTok at <a href="https://www.tiktok.com/@theraptors1711">@raptors1711</a></li>
-					<li>Friend us on Facebook at <a href="https://facebook.com/raptors1711">raptors1711</a></li>
-					<li>Follow us on Twitter at <a href="https://twitter.com/Team1711Raptors">Team1711Raptors</a></li>
-					<li>Check out our code on GitHub at <a href="https://github.com/frc1711">frc1711</a></li>
+					<li>Email us at <a rel="noopener noreferrer" href="mailto:raptors1711@gmail.com">raptors1711@gmail.com</a>.</li>
+					<li>Find us on Instagram at <a rel="noopener noreferrer" href="https://instagram.com/raptors1711">@raptors1711</a></li>
+					<li>Follow us on TikTok at <a rel="noopener noreferrer" href="https://www.tiktok.com/@theraptors1711">@raptors1711</a></li>
+					<li>Friend us on Facebook at <a rel="noopener noreferrer" href="https://facebook.com/raptors1711">raptors1711</a></li>
+					<li>Follow us on Twitter at <a rel="noopener noreferrer" href="https://twitter.com/Team1711Raptors">Team1711Raptors</a></li>
+					<li>Check out our code on GitHub at <a rel="noopener noreferrer" href="https://github.com/frc1711">frc1711</a></li>
 				</ul>
 			</Page>
 		);
